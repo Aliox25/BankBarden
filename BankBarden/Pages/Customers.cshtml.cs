c9830611b3fa825b5a67b904d63bf2c3b98289e2
Alex Bardén <alexbarden25@gmail.com>
2025-03-13 12:57:47 +0100

    Fixte sorting on customers!!!

--- BankBarden/Pages/Customers.cshtml.cs
+++ BankBarden/Pages/Customers.cshtml.cs
@@ -2,6 +2,7 @@ using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using BankBarden.ViewModels;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace BankBarden.Pages
 {
@@ -17,18 +18,53 @@ namespace BankBarden.Pages
 
         public List<AllCustomersViewModel> Customers { get; set; }
         public string Country { get; set; }
-        public void OnGet(string coun)
+        public void OnGet(string coun, string sortColumn, string sortOrder)
         {
             Country = coun;
-            Customers = _dbContext.Customers
-                .Where(c => c.Country == coun)
+            var quary = _dbContext.Customers
+            .Select(c => new AllCustomersViewModel
+            {
+                Id = c.CustomerId,
+                Name = c.Givenname,
+                City = c.City,
+                Country = c.Country
+            });
+
+            if (Country != null)
+            {
+                quary = _dbContext.Customers
+                .Where(c => c.Country == Country)
                 .Select(c => new AllCustomersViewModel
                 {
                     Id = c.CustomerId,
-                    Namn = c.Givenname,
-                    City = c.City
-                })
-                .ToList();
+                    Name = c.Givenname,
+                    City = c.City,
+                    Country = c.Country
+                });
+
+            }
+            if (sortColumn != null && sortOrder != null)
+            {
+                if (sortColumn == "Id")
+                    if (sortOrder == "asc")
+                        quary = quary.OrderBy(c => c.Id);
+                    else if (sortOrder == "desc")
+                        quary = quary.OrderByDescending(c => c.Id);
+
+
+                if (sortColumn == "Name")
+                    if (sortOrder == "asc")
+                        quary = quary.OrderBy(c => c.Name);
+                    else if (sortOrder == "desc")
+                        quary = quary.OrderByDescending(c => c.Name);
+
+                if (sortColumn == "City")
+                    if (sortOrder == "asc")
+                        quary = quary.OrderBy(s => s.City);
+                    else if (sortOrder == "desc")
+                        quary = quary.OrderByDescending(s => s.City);
+            }
+            Customers = quary.ToList();
         }
     }
 }
--- BankBarden/ViewModels/AllCustomersViewModel.cs
+++ BankBarden/ViewModels/AllCustomersViewModel.cs
@@ -3,7 +3,8 @@
     public class AllCustomersViewModel
     {
         public int Id { get; set; }
-        public string Namn { get; set; }
+        public string Name { get; set; }
         public string City { get; set; }
+        public string Country { get; set; }
     }
 }
