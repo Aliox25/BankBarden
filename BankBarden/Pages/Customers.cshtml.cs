a780232252ff530ebfd32435e2a4847bc538973c
Alex Bardén <alexbarden25@gmail.com>
2025-03-13 11:50:42 +0100

    fixt tabel for customers in spesific country

--- BankBarden/Pages/Customers.cshtml.cs
+++ BankBarden/Pages/Customers.cshtml.cs
@@ -1,12 +1,33 @@
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BankBarden.ViewModels;
 
 namespace BankBarden.Pages
 {
     public class CustomersModel : PageModel
     {
-        public void OnGet()
+        private readonly BankAppDataContext _dbContext;
+
+
+        public CustomersModel(BankAppDataContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public List<AllCustomersViewModel> Customers { get; set; }
+
+        public void OnGet(string coun)
+        {
+            Customers = _dbContext.Customers
+                .Where(c => c.Country == coun)
+                .Select(c => new AllCustomersViewModel
+                {
+                    Id = c.CustomerId,
+                    Namn = c.Givenname,
+                    City = c.City
+                })
+                .ToList();
         }
     }
 }
---
+++ BankBarden/ViewModels/AllCustomersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BankBarden.ViewModels
+{
+    public class AllCustomersViewModel
+    {
+        public int Id { get; set; }
+        public string Namn { get; set; }
+        public string City { get; set; }
+    }
+}
