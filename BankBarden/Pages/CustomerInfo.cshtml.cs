da9ca525dca3107f173dd6ba813261bd4cd4845a
Alex Bardén <alexbarden25@gmail.com>
2025-03-14 10:23:48 +0100

    Add customer info site and view model

--- BankBarden/Pages/CustomerInfo.cshtml.cs
+++ BankBarden/Pages/CustomerInfo.cshtml.cs
@@ -1,3 +1,5 @@
+using BankBarden.ViewModels;
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace BankBarden.Pages
 {
     public class CustomerInfoModel : PageModel
     {
+        private readonly BankAppDataContext _dbContext;
+
+        public CustomerInfoModel(BankAppDataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public List<CustomrtInfoViewmodel> Customer {  get; set; }
         public void OnGet()
         {
+
         }
     }
 }
---
+++ BankBarden/ViewModels/CustomrtInfoViewmodel.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLayer.Models;
+
+namespace BankBarden.ViewModels
+{
+    public class CustomrtInfoViewmodel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Account> Accounts { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
