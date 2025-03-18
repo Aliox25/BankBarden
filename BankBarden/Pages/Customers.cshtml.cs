77a06c647bdf57968d23798d9e11ceeff085e184
Alex Bardén <alexbarden25@gmail.com>
2025-03-18 13:26:25 +0100

    Start to add pagination to customers

--- BankBarden/Pages/Customers.cshtml.cs
+++ BankBarden/Pages/Customers.cshtml.cs
@@ -20,9 +20,26 @@ namespace BankBarden.Pages
 
         public List<AllCustomersViewModel> Customers { get; set; }
         public string Country { get; set; }
-        public void OnGet(string coun, string sortColumn, string sortOrder)
+        public string Column { get; set; }
+        public string Order { get; set; }
+        public int CurrentPage { get; set; }
+        public void OnGet(string coun, string sortColumn, string sortOrder, int pageNumb)
         {
             Country = coun;
+
+            if (sortColumn == null)
+                sortColumn = "Id";
+            Column = sortColumn;
+
+            if (sortOrder == null)
+                sortOrder = "asc";
+            Order = sortOrder;
+
+            if (pageNumb == 0)
+                pageNumb = 1;
+            CurrentPage = pageNumb;
+
+            
             var quary = _dbContext.Customers
             .Select(c => new AllCustomersViewModel
             {
@@ -45,27 +62,25 @@ namespace BankBarden.Pages
                 });
 
             }
-            if (sortColumn != null && sortOrder != null)
-            {
-                if (sortColumn == "Id")
-                    if (sortOrder == "asc")
-                        quary = quary.OrderBy(c => c.Id);
-                    else if (sortOrder == "desc")
-                        quary = quary.OrderByDescending(c => c.Id);
 
+            if (sortColumn == "Id")
+                if (sortOrder == "asc")
+                    quary = quary.OrderBy(c => c.Id);
+                else if (sortOrder == "desc")
+                    quary = quary.OrderByDescending(c => c.Id);
 
-                if (sortColumn == "Name")
-                    if (sortOrder == "asc")
-                        quary = quary.OrderBy(c => c.Name);
-                    else if (sortOrder == "desc")
-                        quary = quary.OrderByDescending(c => c.Name);
+            if (sortColumn == "Name")
+                if (sortOrder == "asc")
+                    quary = quary.OrderBy(c => c.Name);
+                else if (sortOrder == "desc")
+                    quary = quary.OrderByDescending(c => c.Name);
 
-                if (sortColumn == "City")
-                    if (sortOrder == "asc")
-                        quary = quary.OrderBy(s => s.City);
-                    else if (sortOrder == "desc")
-                        quary = quary.OrderByDescending(s => s.City);
-            }
+            if (sortColumn == "City")
+                if (sortOrder == "asc")
+                    quary = quary.OrderBy(s => s.City);
+                else if (sortOrder == "desc")
+                    quary = quary.OrderByDescending(s => s.City);
+            
             Customers = quary.ToList();
         }
     }
--- BankBarden/Pages/Shared/Employees.cshtml.cs
+++ BankBarden/Pages/Shared/Employees.cshtml.cs
@@ -3,6 +3,9 @@ using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
 
 namespace BankBarden.Pages.Shared
 {
@@ -20,6 +23,51 @@ namespace BankBarden.Pages.Shared
         public List<EmployeeViewModule> employees { get; set; }
         public void OnGet()
         {
+
+            //var quary = _dbContext.Users
+            //.Include(u => u.UserName)
+            //.Select(c => new EmployeeViewModule
+            //{
+            //    UserName = c.UserName,
+            //    UserRoll = c.UserRoll
+
+            //});
+
+            //if (Country != null)
+            //{
+            //    quary = _dbContext.Customers
+            //    .Where(c => c.Country == Country)
+            //    .Select(c => new AllCustomersViewModel
+            //    {
+            //        Id = c.CustomerId,
+            //        Name = c.Givenname,
+            //        City = c.City,
+            //        Country = c.Country
+            //    });
+
+            //}
+            //if (sortColumn != null && sortOrder != null)
+            //{
+            //    if (sortColumn == "Id")
+            //        if (sortOrder == "asc")
+            //            quary = quary.OrderBy(c => c.Id);
+            //        else if (sortOrder == "desc")
+            //            quary = quary.OrderByDescending(c => c.Id);
+
+
+            //    if (sortColumn == "Name")
+            //        if (sortOrder == "asc")
+            //            quary = quary.OrderBy(c => c.Name);
+            //        else if (sortOrder == "desc")
+            //            quary = quary.OrderByDescending(c => c.Name);
+
+            //    if (sortColumn == "City")
+            //        if (sortOrder == "asc")
+            //            quary = quary.OrderBy(s => s.City);
+            //        else if (sortOrder == "desc")
+            //            quary = quary.OrderByDescending(s => s.City);
+            //}
+            //Customers = quary.ToList();
         }
     }
 }
